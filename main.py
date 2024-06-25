511afc6d8e5f3fa6dd16df0f056dc44c5ea896a0
Roman Pomazan <romapomazan1310@gmail.com>
2024-06-25 11:24:42 +0300

    Added parsing novel images
    - Updated '.gitignore' to include 'media/' directory for storing downloaded novel images
    - Updated 'main.py' to create all the necessary directories
    - Added 'get_novel_image_url' function in 'novel_utils.py
    - Integrated image downloading into 'novel_parser.py'

--- main.py
+++ main.py
@@ -8,15 +8,20 @@ def main() -> None:
     WEBSITE_BASE_URL = "https://animestuff.me/"
     NOVEL_BASE_URL = "https://animestuff.me/docs/assets/html/"
     HTML_FILES_DIR = "html_files"
+    MEDIA_DIR = "media"
     DATA_DIR = "data"
     DATA_FILE = os.path.join(DATA_DIR, "data.json")
     NOVELS_FILE = os.path.join(DATA_DIR, "all_novels_dict.json")
 
+    # Create necessary directories
+    os.makedirs(HTML_FILES_DIR, exist_ok=True)
+    os.makedirs(MEDIA_DIR, exist_ok=True)
     os.makedirs(DATA_DIR, exist_ok=True)
 
     get_all_novels(WEBSITE_BASE_URL, NOVEL_BASE_URL, NOVELS_FILE)
     download_novel_html_files(NOVELS_FILE, HTML_FILES_DIR)
-    get_data_from_html_files(HTML_FILES_DIR, NOVELS_FILE, DATA_FILE)
+    get_data_from_html_files(NOVEL_BASE_URL, HTML_FILES_DIR, MEDIA_DIR,
+                             NOVELS_FILE, DATA_FILE)
 
 
 if __name__ == "__main__":
