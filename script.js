a77cb5f6c1c9e239544cc78fb2402c15c1e56461
Roman Pomazan <romapomazan1310@gmail.com>
2024-06-26 17:16:21 +0300

    Added initial implementation of data view

---
+++ script.js
@@ -0,0 +1,24 @@
+fetch("data/data.json")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(novels) {
+    let placeholder = document.querySelector("#data-output");
+    let out = "";
+    for(let novel of novels) {
+      out += `
+        <tr>
+          <td> <img src='${novel.image}'> </td>
+
+          <td>${novel.title}</td>
+          <td>${novel.status}</td>
+          <td>${novel.synopsis}</td>
+          <td>${novel.genres}</td>
+          <td>${novel.num_volumes}</td>
+          <td>${novel.url}</td>
+        </tr>
+      `;
+    }
+
+    placeholder.innerHTML = out;
+  })
